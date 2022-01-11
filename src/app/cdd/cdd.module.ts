fb52aa5f8222589d65ad4a13b5e8f6ab2b637fbe
Anaglebia <bia22.sp@icloud.com>
2022-01-11 14:05:45 -0300

    Adição do json

--- src/app/cdd/cdd.module.ts
+++ src/app/cdd/cdd.module.ts
@@ -1,15 +1,9 @@
+import { AppMaterialModule } from './../compartilhada/app-material.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { CddRoutingModule } from './cdd-routing.module';
 import { ClassesComponent } from './classes/classes.component';
-import {MatButtonModule} from '@angular/material/button';
-
-
 
 
 
@@ -21,11 +15,7 @@ import {MatButtonModule} from '@angular/material/button';
   imports: [
     CommonModule,
     CddRoutingModule,
-    MatTableModule,
-    MatCardModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-  ]
+    AppMaterialModule,
+]
 })
 export class CddModule { }
--- src/app/cdd/classes/classes.component.ts
+++ src/app/cdd/classes/classes.component.ts
@@ -10,17 +10,7 @@ export class ClassesComponent implements OnInit {
 
   //arrei de livros
   livrosGeneros:Generos[]=[
-       {_idGeneros:"0", nomeGenero:"Generalidades", decimalGenero:"00 ", livrosGenero:423},
-       {_idGeneros:"1", nomeGenero:"Filosofia e pscologia", decimalGenero:"00 ", livrosGenero:125},
-       {_idGeneros:"2", nomeGenero:"Religião", decimalGenero:"00 ", livrosGenero:366},
-       {_idGeneros:"3", nomeGenero:"Ciencias socias", decimalGenero:"00 ", livrosGenero:203},
-       {_idGeneros:"4", nomeGenero:"Linguas", decimalGenero:"00 ", livrosGenero:123},
-       {_idGeneros:"5", nomeGenero:"Ciencias naturais", decimalGenero:"00 ", livrosGenero:523},
-       {_idGeneros:"6", nomeGenero:"Tecnologia e ciencias Aplicadas", decimalGenero:"00 ", livrosGenero:325},
-       {_idGeneros:"7", nomeGenero:"Artes", decimalGenero:"00 ", livrosGenero:523},
-       {_idGeneros:"8", nomeGenero:"Literatura Retorica", decimalGenero:"00 ", livrosGenero:655},
-       {_idGeneros:"9", nomeGenero:"Geografia História e Biologia", decimalGenero:"00 ", livrosGenero:788},
-
+       
   ];
 
   //o que vai ser mostrado na tabela
